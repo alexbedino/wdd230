59342adbdf579f7920cc47466d9e134141a0df0a
alexbedino <alex.bedino@gmail.com>
2022-09-16 23:22:05 +0700

    Added Javascript commands at the bottom of the doc

---
+++ scripts/myscripts.js
@@ -0,0 +1,5 @@
+const d = new Date();
+document.getElementById("demo").innerHTML = d.getFullYear();
+
+console.log(document.lastModified)
+document.querySelector("#lastmod").textContent = document.lastModified
