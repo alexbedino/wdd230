113e4183ec5262235d526682885800715ca486d2
alexbedino <alex.bedino@gmail.com>
2022-09-18 07:13:31 +0700

    Added year variable

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,5 +1,5 @@
 const d = new Date();
-document.getElementById("demo").innerHTML = d.getFullYear();
+document.getElementById("year").innerHTML = d.getFullYear();
 
 console.log(document.lastModified)
 document.querySelector("#lastmod").textContent = document.lastModified
