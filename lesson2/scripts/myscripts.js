6358fce97e42ee2deac56f9424f2a1b345975dc2
alexbedino <alex.bedino@gmail.com>
2022-09-24 23:42:29 +0700

    Created design principles portal

---
+++ lesson2/scripts/myscripts.js
@@ -0,0 +1,5 @@
+const d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
+
+console.log(document.lastModified)
+document.querySelector("#lastmod").textContent = document.lastModified
