9081667967747bd1bf9f6668a7a36ede05608998
alexbedino <alex.bedino@gmail.com>
2022-10-09 23:08:55 +0700

    button

--- chamberofcommerce/js/script.js
+++ chamberofcommerce/js/script.js
@@ -8,3 +8,11 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
+
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
