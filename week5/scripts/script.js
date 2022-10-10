e7fce4f553dec03415b3720232172166ffaa669e
alexbedino <alex.bedino@gmail.com>
2022-10-10 20:41:26 +0700

    bom committ

---
+++ week5/scripts/script.js
@@ -0,0 +1,26 @@
+const list = document.querySelector('ol');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';
+
+  if (myItem !== "") {
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+    input.focus();
+}
+});
