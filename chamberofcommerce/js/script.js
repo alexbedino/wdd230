fcf0ca526458affc4a5f8b688712bad9e8c165f5
alexbedino <alex.bedino@gmail.com>
2022-10-13 22:52:18 +0700

    added additional elements and javascript

--- chamberofcommerce/js/script.js
+++ chamberofcommerce/js/script.js
@@ -16,3 +16,18 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+// creating the show me message for Monday and Tuesday
+
+const today = new Date();
+const dayNumber = today.getDay();
+const element = document.getElementById("message");
+if (dayNumber == 1 || dayNumber == 2 ) {
+	element.classList.add("showme")
+
+} else {
+	element.classList.add("hideme")
+
+}
