f7db55654aa04bed76de8f67953b1329d3d2b81c
alexbedino <alex.bedino@gmail.com>
2022-11-18 21:08:29 +0700

    weather app updated info

--- chamberofcommerce/js/windchill.js
+++ chamberofcommerce/js/windchill.js
@@ -1,7 +1,7 @@
 
-const tempNumber = parseFloat(document.getElementById('temp').textContent);
+const tempNumber = parseFloat(document.getElementById('currentTemp').textContent);
 
-const speedNumber = parseFloat(document.getElementById('speed').textContent);
+const speedNumber = parseFloat(document.getElementById('windSpeed').textContent);
 
 let windchill = 35.74 + (0.6215*tempNumber) - (35.75*(Math.pow(speedNumber,0.16))) + (0.4275*tempNumber*Math.pow(speedNumber,0.16));
 windchill = Math.round(windchill);
@@ -12,4 +12,3 @@ if(tempNumber<= 50 && speedNumber >3){
     document.getElementById("chill").textContent = "No Wind Chill"
 }
 
-`Welcome back ${firstName}! You last visited on <strong>${lvdate}</strong>.`
